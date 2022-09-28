20d7a55922a27b91847dde402259bd8df20222ca
Vaaaleri <109743037+Vaaaleri@users.noreply.github.com>
2022-09-28 16:39:59 +0300

    Initial commit

---
+++ project/newapp/migrations/0002_alter_author_user_rate.py
@@ -0,0 +1,18 @@
+
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('NewsPaper', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='author',
+            name='user_rate',
+            field=models.IntegerField(default=0),
+        ),
+    ]
